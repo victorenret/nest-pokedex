f069f2cb77d2e282bb194b427679198b5e517e05
Victor Retamal <victor.retamal@xintec.cl>
2023-01-23 15:06:04 -0300

    Antes de insertar por lote

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -11,11 +12,13 @@ import { CommonModule } from './common/common.module';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot('mongodb://localhost:27025/nest-pokemon'),
 
     PokemonModule,
 
     CommonModule,
+
+    SeedModule,
   ],
 })
 export class AppModule {}
