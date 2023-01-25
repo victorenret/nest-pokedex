4603097711666c8229cde15e28bbb6dd99d8fad4
Victor Retamal <victor.retamal@xintec.cl>
2023-01-25 15:51:27 -0300

    Añadiendo paginacion para buscar pokemons

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsOptional, IsPositive, Min } from 'class-validator';
+
+
+export class PaginationDto {
+
+    @IsOptional()
+    @IsPositive()
+    @Min(1)
+    limit?: number;
+
+    @IsOptional()
+    @IsPositive()
+    offset?: number;
+    
+}
--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,12 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      whitelist: true, 
+      forbidNonWhitelisted: true, 
+      transform: true,
+      transformOptions:{
+        enableImplicitConversion:true
+      }
     }),
   );
 
