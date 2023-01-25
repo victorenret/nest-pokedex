5f332a8c9e5d020a02dfcc54aec85ee09846f644
Victor Retamal <victor.retamal@xintec.cl>
2023-01-25 16:12:24 -0300

    agregando variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,17 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27025/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
@@ -21,4 +24,11 @@ import { SeedModule } from './seed/seed.module';
     SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor(){
+    console.log( process.env );
+    
+  }
+
+}
